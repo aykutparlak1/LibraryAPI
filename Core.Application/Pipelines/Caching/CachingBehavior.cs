157288c334d4624dfb4ec04a55900456331a60de
aykutparlak1 <aykutparlak20@gmail.com>
2023-09-22 12:06:13 +0300

    ref

---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,24 @@
+﻿using LibraryAPI.Application.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Application.Pipelines.Caching
+{
+    public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ICachableRequest
+    {
+
+        private readonly IDistributedCache _cache;
+
+        public CachingBehavior(IDistributedCache cache, IConfiguration configuration)
+        {
+            _cache = cache;
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LibraryAPI.Application/Interfaces/ICachableRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Application.Interfaces
+{
+    public interface ICachableRequest
+    {
+        string CacheKey { get; }
+        TimeSpan? SlidingExpiration { get; }
+    }
+}
