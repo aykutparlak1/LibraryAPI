bfd7f5728dcc0683383211f068c00720b5f8e068
aykutparlak1 <aykutparlak20@gmail.com>
2023-10-06 16:31:57 +0300

    2

--- LibraryAPI.WebAPI/Controllers/BaseController.cs
+++ LibraryAPI.WebAPI/Controllers/BaseController.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LibraryAPI.WebAPI.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize]
-    public class BaseController : ControllerBase
-    {            
-    }
+namespace LibraryAPI.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class BaseController : ControllerBase
+{            
 }
